c01a70158408c69c7a83693eb54851b9c39f5cbd
SD <sashaluckyman@gmail.com>
2019-04-20 22:03:10 -0700

    add firebase for auth

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import redusers from './reducers';
+import firebase from 'firebase';
+const firebaseConfig = require('./auth/auth')
+
+
+class App extends Component {
+
+    componentDidMount (){
+        firebase.initializeApp(firebaseConfig);
+    }
+    
+    render() {
+        return (
+            <Provider store={createStore(redusers)}>
+                <View>
+                    <Text>some text to render</Text>
+                </View>
+            </Provider>
+            
+        )
+    }
+}
+
+export default App;
