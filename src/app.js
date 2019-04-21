97a244614eb56d3e24a6e389354c20eb0dad534c
SD <sashaluckyman@gmail.com>
2019-04-20 22:08:13 -0700

    fix firebase file

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import redusers from './reducers';
 import firebase from 'firebase';
-const firebaseConfig = require('./auth/auth')
+const firebaseConfig = require('./auth/firebase_config')
 
 
 class App extends Component {
