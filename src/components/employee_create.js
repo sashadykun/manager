b14c235a6f2b7a875bfbaa5680497aba563b4a08
SD <sashaluckyman@gmail.com>
2019-05-10 20:08:19 -0700

    add picker to show day selection

--- src/components/employee_create.js
+++ src/components/employee_create.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Picker } from 'react-native';
 import { connect } from 'react-redux';
 import { employUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common'
@@ -29,7 +30,19 @@ class EmployeeCreate extends Component {
                 </CardSection>
 
                 <CardSection>
-                    
+                    <Picker
+                        style={{ flex: 1 }}
+                        selectedValue={this.props.shift}
+                        onValueChange={value => this.props.employUpdate({ prop: 'shift', value })}
+                    >
+                        <Picker.Item label="Monday" value="Monday" />
+                        <Picker.Item label="Tuesday" value="Tuesday" />
+                        <Picker.Item label="Wednesday" value="Wednesday" />
+                        <Picker.Item label="Thursday" value="Thursday" />
+                        <Picker.Item label="Friday" value="Friday" />
+                        <Picker.Item label="Saturday" value="Saturday" />
+                        <Picker.Item label="Sunday" value="Sunday" />
+                    </Picker>
                 </CardSection>
 
                 <CardSection>
